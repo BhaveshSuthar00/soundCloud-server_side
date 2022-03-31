c866da67f4594dfc55218f7fc91d577af87052f2
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-03-31 18:27:14 +0530

    change

--- src/Middleware/Upload.js
+++ src/Middleware/Upload.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 
 var storage = multer.diskStorage({
     destination : function(req, file, cb) {
-        cb(null,  "uploads/")
+        cb(null,  "../uploads/")
     },
     filename : function(req,file, cb){
         let ext = path.extname(file.originalname);
