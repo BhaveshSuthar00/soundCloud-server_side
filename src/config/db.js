3b50c425ab9e9a736b61fc9659685bcd8a2fb15a
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-03-31 16:53:13 +0530

    addded

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const connect = async () => {
-    // return mongoose.connect("mongodb+srv://saurabh:saurabh@cluster0.ypmi8.mongodb.net/soundcloudDB?retryWrites=true&w=majority").then(()=> {
-    //     // console.log("Connected")
-    // })
     return mongoose.connect(process.env.MONGODBURL)
     .catch((e)=>{
         console.log(e)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require("express")
 require("dotenv").config();
-
-
 const app = express()
+const connect = require("./config/db")
 app.use(express.json())
 const PORT = process.env.PORT || 6767
 
@@ -19,5 +18,9 @@ app.use("/liked",likedController)
 app.use('/songpost', Songs)
 app.use("/playlist",playlistController)
 app.use("/library",libraryController)
+app.listen(PORT, async ()=>{
+    await connect()
+    console.log("listening on port 2000")
+})
 
 module.exports=app
