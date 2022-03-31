504556efaefa63cf84f2e773e6b9df5a1666b41a
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-03-31 16:43:26 +0530

    added .env

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const connect = async () => {
     // return mongoose.connect("mongodb+srv://saurabh:saurabh@cluster0.ypmi8.mongodb.net/soundcloudDB?retryWrites=true&w=majority").then(()=> {
     //     // console.log("Connected")
     // })
-    return mongoose.connect('mongodb+srv://bhavesh:bhavesh@cluster0.vxlup.mongodb.net/soundCloud?retryWrites=true&w=majority')
+    return mongoose.connect(process.env.MONGODBURL)
     .catch((e)=>{
         console.log(e)
     })
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express")
+require("dotenv").config();
 
 
 const app = express()
 app.use(express.json())
-
 const PORT = process.env.PORT || 6767
 
 
