1b653f52e5e80ccfb83117e000f1cb7925d25aef
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-04-02 12:47:16 +0530

    added more routes in user.controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,13 +6,25 @@ const router =  express.Router()
 
 router.post("/post",async(req,res)=>{
     try{
-        const user = await User.create(req.body)
+        const user = await User.create({
+            email: req.body.email,
+            password : req.body.password,
+            userName : req.body.name
+        })
         return res.status(200).send(user)
     }catch(err){
         return res.status(400).send(err.message)
     }
 })
-
+router.get('/all', async (req, res) => {
+    try {
+        const user = await User.find().lean().exec();
+        return res.status(200).send(user)
+    }
+    catch(err){
+        return res.status(400).send({message : "users not found"})
+    }
+})
 
 router.get("/login",async(req,res)=>{
     try{
--- src/model/user.model.js
+++ src/model/user.model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema(
     {
-      email: { type: String, required: false },
-      password: { type: String, required: false },
-      
+      email: { type: String, required: true },
+      password: { type: String, required: true },
+      userName : { type: String, required: true}
     },
     {
       versionKey: false,
